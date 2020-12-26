a0af94f95f6184b1b5fa45301926d873b8e24593
Alde Rojas <hello@alde.io>
2020-12-26 10:21:34 -0600

    Display RGB stripes on TFT

--- main.c
+++ main.c
@@ -1,24 +1,31 @@
+#include <stdbool.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
 #include "serial.h"
+#include "debug.h"
+#include "ili9340.h"
 
 
-static void initialize(void);
+ili9340_t tft = {&spi, B2, D6, D5};
+
+
+static void initialize(void) {
+  serial_init(&serial0, 115200);
+  spi_init(&spi);
+  ili9340_init(&tft);
+}
 
 
 int main(void) {
   initialize();
 
-  while (1) {
-    serial_printf(&serial0, "Hello, world\r\n");
-    _delay_ms(100);
-  }
+  ili9340_memory_write(&tft);
+  ili9340_fill(&tft, (ili9340_color_t){ 0x3f, 0, 0 }, 25600);
+  ili9340_fill(&tft, (ili9340_color_t){ 0, 0x3f, 0 }, 25600);
+  ili9340_fill(&tft, (ili9340_color_t){ 0, 0, 0x3f }, 25600);
+
+  while (1);
 
   return 0;
-}
-
-
-static void initialize(void) {
-  serial_init(&serial0, 115200);
 }
